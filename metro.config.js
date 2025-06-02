08597f0f205e5a6d609179a0648f916a73378d44
Elias Tazartes <elias.tazartes@gmail.com>
2025-06-02 12:35:03 +0200

    add csca masterlist in bundle

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+defaultConfig.resolver.assetExts.push("pem");
+
 defaultConfig.resolver.nodeModulesPaths = [
   ...defaultConfig.resolver.nodeModulesPaths,
   path.resolve(__dirname, "modules"),
