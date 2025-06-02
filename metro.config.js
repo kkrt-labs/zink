b05579e77d1cb34b96b20868963652e4bf0b9eb1
Elias Tazartes <elias.tazartes@gmail.com>
2025-06-02 14:24:27 +0200

    remove useless config line

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,6 @@ const path = require("path");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
-defaultConfig.resolver.assetExts.push("pem");
-
 defaultConfig.resolver.nodeModulesPaths = [
   ...defaultConfig.resolver.nodeModulesPaths,
   path.resolve(__dirname, "modules"),
